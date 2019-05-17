611438a24bb433e51c4b6f3e4003ffe62c07b11c
vincenzo <qwauknation@yahoo.com>
2019-05-17 13:19:51 -0400

    edit #2 WOAT

--- src/main/java/bullhorn/demo/Message.java
+++ src/main/java/bullhorn/demo/Message.java
@@ -2,17 +2,69 @@ package bullhorn.demo;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @NotNull
     private String title;
+    @NotNull
     private String content;
-    private int postedDate;
+    @NotNull private String postedDate;
+
+    @NotNull
     private String postedBy;
 
-    @OneToMany(mappedBy = "Message", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<>
+    public Message() {
+    }
+
+    public Message(@NotNull String title, @NotNull String content, @NotNull String postedDate, @NotNull String postedBy) {
+        this.title = title;
+        this.content = content;
+        this.postedDate = postedDate;
+        this.postedBy = postedBy;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(String postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public String getPostedBy() {
+        return postedBy;
+    }
+
+    public void setPostedBy(String postedBy) {
+        this.postedBy = postedBy;
+    }
 }
